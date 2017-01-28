48d8c7f8d11d6ff29cce13775edc3532a856f0b2
Bartosz Borowiecki <bartosz.s.borowiecki@gmail.com>
2017-01-28 15:41:40 +0100

    Running the game server

--- server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var PongHttpServer_1 = require("./Scripts/HttpServer/PongHttpServer");
-var httpServer = new PongHttpServer_1.PongHttpServer();
-httpServer.run();
-//# sourceMappingURL=server.js.map
