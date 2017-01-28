5a899ea5b60befe8941ab5addf9fb78633b635ac
Bartosz Borowiecki <bartosz.s.borowiecki@gmail.com>
2017-01-28 15:41:51 +0100

    Running the game server

--- server.ts
+++ server.ts
@@ -1,5 +1,11 @@
 ﻿import { PongHttpServer } from "./Scripts/HttpServer/PongHttpServer";
+import { GameServer } from "./Scripts/PongServer/GameServer";
 
 let httpServer = new PongHttpServer();
 
 httpServer.run();
+
+let gameServer = new GameServer();
+
+gameServer.run();
+
